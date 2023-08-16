8fe131ced6d7a15fe252327a41271381431021a7
Shrikant Jamale <shrikantj0794@gmail.com>
2023-08-16 16:44:42 +0530

    url change and update

--- Quote Generator/script.js
+++ Quote Generator/script.js
@@ -1,26 +1,19 @@
-const apikey = "YdRnHaJJ86gucfzPeH8Fxw==VLWNPsuNLWTZ6GVW";
-const apiUrl = "https://api.api-ninjas.com/v1/quotes?category=";
-var category = 'happiness'
+let quote = document.getElementById("quote");
+let author = document.getElementById("author");
+let btn = document.getElementById("btn");
 
-async function getquote(apiUrl){
-    const response = await fetch(apiUrl +category + `&X-Api-Key=${apikey}`);
-    var data = await response.json();
+const url = "https://api.quotable.io/random";
 
-    console.log(data);
-}
-getquote(apiUrl)
 
-// var category = 'happiness'
-// .ajax({
-//     method: 'GET',
-//     url: 'https://api.api-ninjas.com/v1/quotes?category=' + category,
-//     headers: { 'X-Api-Key': 'YOUR_API_KEY'},
-//     contentType: 'application/json',
-//     success: function(result) {
-//         console.log(result);
-//     },
-//     error: function ajaxError(jqXHR) {
-//         console.error('Error: ', jqXHR.responseText);
-//     }
-// });
-// ajax();
+let getQuote = () => {
+    fetch(url)
+      .then((data) => data.json())
+      .then((item) => {
+        quote.innerText = item.content;
+        author.innerText = item.author;
+      });
+  };
+
+  
+window.addEventListener("load", getQuote);
+btn.addEventListener("click", getQuote);
