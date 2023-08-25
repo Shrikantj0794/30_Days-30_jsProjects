24fc0948df326b5ae9391fdd34b2187504c69024
Shrikant Jamale <shrikantj0794@gmail.com>
2023-08-25 18:17:57 +0530

    added more css and js

--- Toast Notification or Snack Bar/script.js
+++ Toast Notification or Snack Bar/script.js
@@ -2,3 +2,21 @@ let toastBox = document.getElementById('toastBox');
 let successMsg = '<i class="fa-solid fa-circle-check"></i> Successfully submitted';
 let errorMsg = '<i class="fa-solid fa-circle-xmark"></i> Please fix the error!';
 let invalidMsg = '<i class="fa-solid fa-circle-exclamation"></i> Invalid input, check again';
+
+function showToast(msg) {
+    let toast = document.createElement('div');
+    toast.classList.add('toast');
+    toast.innerHTML = msg;
+    toastBox.appendChild(toast);
+
+    if(msg.includes('Invalid')){
+        toast.classList.add('invalid');
+    }
+    if(msg.includes('error')){
+        toast.classList.add('error');
+    }
+
+    // setTimeout(()=>{
+    //     toast.remove()
+    // },4000)
+}
